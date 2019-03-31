5f179bb01cdacac758b04cf7631440e2eb63ba5a
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-03-31 00:43:05 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -16,8 +16,10 @@ class Application
     elsif req.path.match(/cart/)
       if @@cart.empty?
         resp.write "Your cart is empty"
-      @@cart.each do |item|
-        resp.write "#{item}\n"
+      else
+        @@cart.each do |item|
+          resp.write "#{item}\n"
+        end
       end
     elsif req.path.match(/add/)
       item = req.params["q"]
