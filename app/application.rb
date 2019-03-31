d3d5c9ab8c57a28683420885acf106f31989159e
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-03-31 00:44:48 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -24,6 +24,7 @@ class Application
     elsif req.path.match(/add/)
       item = req.params["q"]
       @@cart << item
+      resp.write "added #{item}"
     else
       resp.write "Path Not Found"
     end
